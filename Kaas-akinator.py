53044e544ac780dfd09567dfcc83ce709fe68aaf
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-09-20 14:09:47 +0200

    De kaas calculator opdracht die hoort bij F1.3.01.L2 - Kaas, kaas en nog eens kaas

---
+++ Kaas-akinator.py
@@ -0,0 +1,31 @@
+print("beantwoord alle vragen met J of N")
+
+question1 = input("is de kaas geel? ")
+if question1 == ("J"):
+    question2 = input("Zitten er gaten in? ")
+    if question2 == ("J"):
+        question3 = input("is de kaas belachelijk duur? ")
+        if question3 == ("J"):
+            print("Uw kaas is Emmenthaler")
+        else:
+            print("Uw kaas is Leerdammer")
+    else:
+        question4 = input("Is uw kaas hard als steen? ")
+        if question4 == ("J")
+            print("Uw kaas is Pamigiano Reggiano")
+        else:
+            print("Uw kaas is Goudse kaas")
+else:
+    question5 = input("Heeft de kaas blauwe schimmels? ")
+    if question5 == ("J")
+        question6 = input("Heeft uw kaas een korst? ")
+        if question6 == ("J")
+            print("Uw kaas is Camembert")
+        else:
+            print("Uw kaas is Mozzarella")
+    else:
+        question7 = input("Heeft uw kaas een korst? ")
+        if question7 == ("J")
+            print("Uw kaas is Bleu de Rochbaron")
+        else:
+            print("Uw kaas is Foume d'Ambert")
